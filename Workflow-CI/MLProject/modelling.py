b450807c521d41653f7c13bd0816c68859fb6435
MuhammadMirzaRalfie <x4veront@gmail.com>
2025-12-06 20:48:38 +0700

    next

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -2,45 +2,30 @@ import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-
 def load_data(path):
     df = pd.read_csv(path)
-
-    # Cek kolom y ada atau tidak
-    if "y" not in df.columns:
-        raise ValueError("Kolom 'y' tidak ditemukan dalam dataset.")
-
     return df
 
-
 def train_model(df):
-    # Pisahkan fitur dan target
     X = df.drop("y", axis=1)
     y = df["y"]
 
-    # Split data
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
+        X, y, test_size=0.2, random_state=42
     )
 
-    # Model
-    model = RandomForestClassifier(
-        n_estimators=150,
-        random_state=42,
-        n_jobs=-1
-    )
-
+    model = RandomForestClassifier(n_estimators=150, random_state=42)
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
 
     return model, acc
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -49,13 +34,14 @@ if __name__ == "__main__":
 
     df = load_data(args.data_path)
 
+    tracking_path = os.path.join(os.getcwd(), "mlruns")
+    mlflow.set_tracking_uri(tracking_path)
+    mlflow.set_experiment("workflow-ci")
+
     with mlflow.start_run():
         model, acc = train_model(df)
 
-        # Log metrics
         mlflow.log_metric("accuracy", acc)
-
-        # Log model
         mlflow.sklearn.log_model(model, artifact_path="model")
 
         print("Training completed. Accuracy:", acc)
